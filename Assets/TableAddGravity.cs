043c2aaaf0d0c926b26babf881b9aea63a2fe2be
Mads Kagemand <counter_use@hotmail.com>
2016-01-29 21:44:15 +0100

    Prototype scene

---
+++ Assets/TableAddGravity.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TableAddGravity : MonoBehaviour {
+
+
+   
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        
+	}
+
+    void OnTriggerEnter() {
+
+        if (tag == "Player")
+            Debug.Log("hejt");
+    }
+}
