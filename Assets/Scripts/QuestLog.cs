bf2c710b3b8e7eeef607af37476d4f0575124de5
Mossa Merhi <cgmossa@gmail.com>
2016-01-30 09:53:20 +0100

    Mossas attempt at a quest log, needs more work. committed for pull reasons

---
+++ Assets/Scripts/QuestLog.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quest
+{
+    string QuestDesc;
+    bool QuestStatus = false;
+
+    TextMesh QuestMesh;
+
+    public Quest()
+    {
+        this.QuestDesc = "This is a new quest!";
+        this.QuestStatus = false;
+
+    }
+
+    public Quest(string questDesc)
+    {
+        this.QuestDesc = questDesc;
+    }
+
+    public void BuildMesh()
+    {
+        this.QuestMesh = new TextMesh();
+        this.QuestMesh.text = this.QuestDesc;
+
+    }
+
+}
+
+public class QuestLog : MonoBehaviour {
+
+    //List of all quests 
+    Quest[] AllQuests = new Quest[2];
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+}
---
+++ Assets/Scripts/QuestPlane.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuestPlane : MonoBehaviour {
+    
+    private MeshRenderer planeRenderer;
+
+    [SerializeField]
+    private Texture DoneTexture;
+
+
+    void Start()
+    {
+        planeRenderer = gameObject.GetComponent<MeshRenderer>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        //Object doneTexture = Resources.Load("./Materials/quest_dummy_texture_DONE.png");
+
+        planeRenderer.material.mainTexture = this.DoneTexture;
+
+    }
+}
